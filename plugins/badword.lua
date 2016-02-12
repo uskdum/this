6f234aa846c83435d65c3117092b0c4d7aa1f6d1
uskdum <8uskdum@30mail.ir>
2016-02-12 16:45:13 +0330

    Create badword.lua

---
+++ plugins/badword.lua
@@ -0,0 +1,65 @@
+do
+
+local function run(msg, matches)
+   if not is_admin or is_sudo(msg) then
+     local chat = get_receiver(msg)
+     local user = "user#id"..msg.from.id
+         send_large_msg(chat, 'BadWord Detected! ')
+         send_msg(chat, 'Kicking...!')
+         chat_del_user(chat, user, ok_cb, true)
+     end
+   end
+
+return {
+patterns = {
+		"koskesh"
+		"kooni"
+		"kiooni"
+		"fuck"
+		"cunt"
+		"pussy"
+		"sex"
+		"kos"
+		"koon"
+		"suck"
+		"Ó˜Ó"
+		"Ó˜Óí"
+		"jooooon"
+		"joooon"
+		"jooon"
+		"joon"
+		"mame"
+		"85"
+		"penis"
+		"dick"
+		"cock"
+		"ÇííÏä"
+		"˜Õ"
+		"˜íÑ"
+		"kir"
+		"˜Õ ääÊ"
+		"˜Õ ÈÇÈÇÊ"
+		"ääÊæ ÇííÏã"
+		"jendeh"
+		"jende"
+		"kirkhar"
+		"˜íÑ ÎÑ"
+		"ãÇÏÑ ÞÍÈå"
+		"madarjende"
+		"jakesh"
+		"ÌÇ˜Ô"
+		  "kir"
+   "kos"
+   "kon"
+   "koni"
+   "ننه جنده"
+   "کس"
+   "کیر"
+   "کون"
+   "کونی"
+   "لاشی"
+   "گاییدمت"
+	}, 
+run = run
+}
+end
