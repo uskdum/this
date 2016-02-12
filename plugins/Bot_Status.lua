a12e44512d2cee57aa097da4b4b7d6a99487e3a3
uskdum <8uskdum@30mail.ir>
2016-02-12 16:40:27 +0330

    Create Bot_Status.lua

---
+++ plugins/Bot_Status.lua
@@ -0,0 +1,31 @@
+do
+-- *** --
+-- 
+-- *** --
+function run(msg, matches)
+local os.time(os.date("!*t")) = os.time(os.date)
+local chat_id = "chat#id"..result.id
+local chatname = result.print_name
+local receiver = get_receiver(msg)
+local first_name = user.first_name
+local print_name = user.print_name
+local user_name = user.user_name
+-- *** --
+local botanswers = {'Bot \n in 'os.time(os.date)' Runned!✅ \n--------\nServer Status📡: \n----------\n🚩True \nPing:👇 \n10.1 \n\n\nFor FeedBack \nUse /feedback [query] \n\n\n\n ✨ New Plugins Soon✊ \n@PokerFace_Dev'}
+  return botanswers[math.random(#botanswers)]
+send_msg(receiver, "شما میتونین از طریق لینک زیر گروه خودرا به گروه های ویژه تبدیل کنید و از امکانت لذت ببرید \n LINK  " ok_cb, false)
+send_large_msg(receiver, 'Bot QR Code :)\n **Link Not Set** ' ok_cb, false)
+send_document(receiver, "./media/Sticker.webp", ok_cb, false)
+send_large_msg(receiver, 'FristName:'..first_name..'\nFullName:'..print_name..'\nUserName: @'..user_name..'' ok_cb, false)
+	print "Bot Status Was Show For Group:"..chatname.." With ID:"..chat_id..":)"
+  end
+-- *** --
+return {
+  patterns = {
+	"^[Ss][Tt][Aa][Tt][Uu][Ss]$"
+	"^!!tgservice (.+)$",
+  }, 
+  run = run 
+}
+end
+-- *** --
